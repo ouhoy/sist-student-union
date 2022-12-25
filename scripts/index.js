f702ce28569e3280719597e5a3f72dba4c39f6b7
ouhoy <kimkinon@gmail.com>
2022-12-25 22:14:29 +0100

    Make Tablet view responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,12 @@
 function $(id, selectAll = false) {
-  return selectAll ? document.querySelectorAll(id) : document.querySelector(id);
+    return selectAll ? document.querySelectorAll(id) : document.querySelector(id);
 }
 
+const eventsMenu = $(".events-menu")
+const showEventsBtn = $(".event-btn")
 
+showEventsBtn.addEventListener("click", function () {
+    this.classList.toggle("event-btn-reshape")
+    eventsMenu.classList.toggle("hidden")
+    showEventsBtn.querySelector("img").classList.toggle("rotate")
+})
