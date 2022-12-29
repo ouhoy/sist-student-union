ddaf3cf80f7a5218a62ad59bbf4d5c1b90449463
ouhoy <kimkinon@gmail.com>
2022-12-29 16:57:00 +0100

    Link window.location with the events menu

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,33 @@ const events = $(".event", true)
 // const articleBody = $(".article-body")
 const articleTitle = $(".article-body-title")
 
+window.onload = function () {
+    const eventId = window.location.hash.split("-").at(-1)
+    if (eventId !== "") {
+        const linkedEvent = document.getElementById(eventId)
+        return setEventsMenu(linkedEvent)
+
+    }
+    events[0].setAttribute("is-active", "")
+}
+
 showEventsBtn.addEventListener("click", function () {
     this.classList.toggle("event-btn-reshape")
     eventsMenu.classList.toggle("hidden")
     showEventsBtn.querySelector("img").classList.toggle("rotate")
 })
+
+
+function setEventsMenu(target) {
+     articleTitle.textContent = target.querySelector("h6").textContent
+
+    const eventTitle = target.querySelector("h6").textContent.replaceAll(" ", "-").toLowerCase()
+    window.location.hash = eventTitle + "-" + target.id;
+    events.forEach(el => {
+        if (el.hasAttribute("is-active")) el.removeAttribute("is-active")
+    })
+    target.setAttribute("is-active", "")
+}
 
 eventsMenu.addEventListener("click", function (e) {
     const selectedEvent = e.target.closest(".event");
