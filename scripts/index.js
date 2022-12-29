22756fd55ca5ceab11ac5d0e9078c71902d56f05
ouhoy <kimkinon@gmail.com>
2022-12-29 14:10:33 +0100

    Make the events menu functional

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,25 @@ function $(id, selectAll = false) {
 
 const eventsMenu = $(".events-menu")
 const showEventsBtn = $(".event-btn")
+const events = $(".event", true)
+// const articleBody = $(".article-body")
+const articleTitle = $(".article-body-title")
 
 showEventsBtn.addEventListener("click", function () {
     this.classList.toggle("event-btn-reshape")
     eventsMenu.classList.toggle("hidden")
     showEventsBtn.querySelector("img").classList.toggle("rotate")
+})
+
+eventsMenu.addEventListener("click", function (e) {
+    const selectedEvent = e.target.closest(".event");
+    if (!selectedEvent || selectedEvent.hasAttribute("is-active")) return;
+    articleTitle.textContent = selectedEvent.querySelector("h6").textContent
+
+    const eventTitle = selectedEvent.querySelector("h6").textContent.replaceAll(" ", "-").toLowerCase()
+    window.location.hash = eventTitle + "-" + selectedEvent.id;
+    events.forEach(el => {
+        if (el.hasAttribute("is-active")) el.removeAttribute("is-active")
+    })
+    selectedEvent.setAttribute("is-active", "")
 })
