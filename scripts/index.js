a489eabcb6a97a8da79c1c7193e9ca9dc149002b
ouhoy <kimkinon@gmail.com>
2022-12-30 19:38:33 +0100

    Add events cards type switch buttons

--- scripts/index.js
+++ scripts/index.js
@@ -7,9 +7,11 @@ const showEventsBtn = $(".event-btn")
 const events = $(".event", true)
 // const articleBody = $(".article-body")
 const articleTitle = $(".article-body-title")
+const eventController = $(".events-controller")
+
+const currentPage = window.location.href.split("/")[4]
 
 window.onload = function () {
-    const currentPage = window.location.href.split("/")[4]
     if (currentPage !== "index.html") return
     const eventId = window.location.hash.split("-").at(-1)
     if (eventId !== "") {
@@ -21,6 +23,7 @@ window.onload = function () {
 }
 
 showEventsBtn.addEventListener("click", function () {
+    if (currentPage == "events.html") return
     this.classList.toggle("event-btn-reshape")
     eventsMenu.classList.toggle("hidden")
     showEventsBtn.querySelector("img").classList.toggle("rotate")
@@ -39,6 +42,7 @@ function setEventsMenu(target) {
 }
 
 eventsMenu.addEventListener("click", function (e) {
+    if (currentPage == "events.html") return
     const selectedEvent = e.target.closest(".event");
     if (!selectedEvent || selectedEvent.hasAttribute("is-active")) return;
     articleTitle.textContent = selectedEvent.querySelector("h6").textContent
@@ -51,3 +55,11 @@ eventsMenu.addEventListener("click", function (e) {
     selectedEvent.setAttribute("is-active", "")
 })
 
+eventController.addEventListener("click", function (e) {
+    if (currentPage !== "events.html") return
+    if (e.target.closest("button")) {
+        eventController.querySelectorAll("button").forEach(el => el.classList.toggle("secondary-btn"))
+
+
+    }
+})
