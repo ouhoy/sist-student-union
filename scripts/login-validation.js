d4fc7b778c3b09801c4e44cac91665956de33f61
ouhoy <kimkinon@gmail.com>
2023-01-21 23:47:22 +0100

    Make login validation

---
+++ scripts/login-validation.js
@@ -0,0 +1,21 @@
+function $(id, selectAll = false) {
+    return selectAll ? document.querySelectorAll(id) : document.querySelector(id);
+}
+
+const userName = document.getElementById("username")
+const password = document.getElementById("password")
+const submitBtn = $("form button")
+
+
+submitBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    if (!userName.value) {
+        console.log("Please enter a valid Username")
+    }
+    if (!password.value) {
+        console.log("Please enter a Password!")
+        return
+    }
+
+    console.table({password: password.value, username: userName.value})
+})
